cb3d1e7b6e503f3705667b41889beea0e1763e04
dev <kyman19940214@gmail.com>
2021-12-16 10:16:58 +0900

    redux-persist 추가

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import todoReducer from './modules/todo';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  todo: todoReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['todo'],
+};
+
+const _persitedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    todo: todoReducer,
-  },
+  reducer: _persitedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: ['persist/PERSIST'],
+    },
+  }),
 });
+
+export const persistor = persistStore(store);
